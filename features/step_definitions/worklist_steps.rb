737e52e48ad9d80577a4bbc089a8ef275b2b6a1b
Steve Millis <steve.millis@digital.landregistry.gov.uk>
2015-08-05 09:40:08 +0100

    Acceptance test

--- features/step_definitions/worklist_steps.rb
+++ features/step_definitions/worklist_steps.rb
@@ -2,7 +2,6 @@ Given(/^I have selected to view the main worklist$/) do
   visit( 'http://localhost:5010')
 end
 
-#When   I have selected to view specific the application list "pab"
 When(/^I have selected to view specific the application list "(.*)"$/) do |type|
     visit( "http://localhost:5010/get_list?appn=#{type}" )
 end
@@ -12,3 +11,11 @@ Then (/^I see the pab application list page$/) do
     page.should have_content("21 September 2014")
 end
 
+Then (/^I see the wob application list page$/) do
+    page.should have_content("WO Bankruptcy Registrations")
+    page.should have_content("30 August 2014")
+end
+
+Then (/^I see the application list page with no waiting apps$/) do
+    page.should have_content("There are no waiting applications")
+end
