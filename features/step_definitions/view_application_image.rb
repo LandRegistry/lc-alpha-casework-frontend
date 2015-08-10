724d7dcbcaac6a903665a0cb8bc2db034b4de0c0
nicola <nicola.andrews@digital.landregisty.gov.uk>
2015-08-10 11:29:50 +0100

    sprint 4 story us005

---
+++ features/step_definitions/view_application_image.rb
@@ -0,0 +1,26 @@
+Given(/^I am on the view application screen$/) do 
+  visit('http://localhost:5010')
+end 
+
+When(/^I have selected to view specific the application list$/) do 
+    visit( "http://localhost:5010/get_list?appn=bank_regn" )
+    find(:xpath,"html/body/div[1]/div/div/div[3]/div/table/tbody/tr[1]/td[1]/a").click
+    
+end 
+
+When(/^the image of the application is displayed I can click on all available pages$/) do 
+    find(:id, "thumbnail_2").click
+    find(:id, "thumbnail_3").click
+    find(:id, "thumbnail_1").click 
+end 
+
+When(/^I am on a page I can zoom out$/) do 
+  find(:xpath, "//img[@title='Zoom Out']").click
+end 
+
+Then(/^I am on a page I can zoom in$/) do
+    find(:id, "thumbnail_2").click   
+    find(:xpath, "//img[@title='Zoom In']").click
+end 
+
+ 
---
+++ features/view_application_image.feature
@@ -0,0 +1,14 @@
+@vai
+
+Feature: View Application Image
+
+As a land charges caserworker
+I want to be able to view an image on a bankruptsy application form
+So that I can ensure that details are captured correctly onto the land charges register
+
+Scenario: Using the land charges system
+Given I am on the view application screen
+When I have selected to view specific the application list
+When the image of the application is displayed I can click on all available pages
+When I am on a page I can zoom out
+Then I am on a page I can zoom in
