8ff3aea97c3c7f11818e2b8b29a3b62f5f7e360e
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-08-26 12:27:57 +0100

    Helper method to create registration

--- features/step_definitions/steps_common.rb
+++ features/step_definitions/steps_common.rb
@@ -8,3 +8,21 @@ require 'json'
 require 'pg'
 
 Capybara.default_driver = :selenium
+
+
+
+def create_registration
+    no_alias = '{"key_number":"9056267","application_type":"PA(B)","application_ref":"9763603","date":"2014-11-12","debtor_name":{"forenames":["Lamar","Sigmund"],"surname":"Effertz"},"debtor_alternative_name":[],"gender":"N/A","occupation":"Ship builder","residence":[{"address_lines":["942 Carley Unions","Cullenberg","Dimitrimouth","Buckinghamshire"],"postcode":"QF47 0HG"}],"residence_withheld":false,"business_address":{"address_lines":["122 Leuschke Creek","Alvaburgh","Fife"],"postcode":"NO03 1EU"},"date_of_birth":"1974-10-03","investment_property":[]}'
+    uri = URI("http://localhost:5004")
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    request = Net::HTTP::Post.new("/registration")
+    request.body = no_alias
+    request["Content-Type"] = "application/json"
+
+    response = http.request(request)
+    data = JSON.parse(response.body)
+
+
+    data['new_registrations'][0]
+end
