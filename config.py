597865ca75b26761fa31949732cdca16228ce5f1
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-04-01 12:20:52 +0100

    Switch to single env vars

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config(object):
     APPLICATION_NAME = 'lc-casework-frontend'
     CASEWORK_FRONTEND_URL = os.getenv('CASEWORK_FRONTEND_URL', "http://localhost:5010")
     CASEWORK_API_URL = os.getenv('CASEWORK_API_URL', "http://localhost:5006")
+    AMQP_URI = os.getenv("AMQP_URI", "amqp://mquser:mqpassword@localhost:5672")
